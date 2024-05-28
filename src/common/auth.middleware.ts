2aa4a9c2ad6d48a9ebf3793470aa597cc003be92
AdkPrtm <andhikapratama685@gmail.com>
2024-05-28 21:41:56 +0800

    fix: middleware if token invalid

--- src/common/auth.middleware.ts
+++ src/common/auth.middleware.ts
@@ -1,4 +1,4 @@
-import { HttpException, Injectable, NestMiddleware } from '@nestjs/common';
+import { HttpException, Injectable, NestMiddleware, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from './prisma.service';
 
@@ -7,22 +7,25 @@ export class AuthMiddleware implements NestMiddleware {
   constructor(
     private jwtService: JwtService,
     private prismaService: PrismaService,
-  ) {}
+  ) { }
 
   async use(req: any, res: any, next: (error?: Error | any) => void) {
     const token = this.extractTokenFromHeader(req);
     if (!token) throw new HttpException('Unauthorized', 401);
 
-    const payload = this.jwtService.verify(token);
-    if (!payload.id) throw new HttpException('Something went wrong', 500);
+    try {
+      const payload = this.jwtService.verify(token);
+      
+      const user = await this.prismaService.user.findUnique({
+        where: { id: payload.id },
+      });
+      if (!user) throw new HttpException('Unauthorized', 401);
 
-    const user = await this.prismaService.user.findUnique({
-      where: { id: payload.id },
-    });
-    if (!user) throw new HttpException('Unauthorized', 401);
-
-    req.user = user;
-    next();
+      req.user = user;
+      next();
+    } catch (err) {
+      throw new UnauthorizedException('Invalid token');
+    }
   }
 
   private extractTokenFromHeader(request: Request): string | undefined {
